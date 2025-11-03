23d39315eaba005e75102a71656e186085ebcfd0
xEm1rald <rostikroi2009@gmail.com>
2025-11-03 22:32:55 +0200

    fix crashes with (pyinstaller --noconsole)

--- __main__.py
+++ __main__.py
@@ -1,5 +1,12 @@
 from app import run
+import sys
+import os
+import logging
 
+if not sys.stdout:
+    sys.stdout = open(os.devnull, "w")
+if not sys.stderr:
+    sys.stderr = open(os.devnull, "w")
 
 if __name__ == '__main__':
     run()
